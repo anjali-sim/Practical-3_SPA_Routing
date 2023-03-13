2d850b87d56d88da174121ce60d0dcfa62b6f123
anjali-sim <anjalighetia123@gmail.com>
2023-03-13 17:54:34 +0530

    :hammer: Implemented the Routing functionality

--- src/script/script.js
+++ src/script/script.js
@@ -17,3 +17,36 @@ buttonBottomToTop.addEventListener("click", function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+// defining object for routes
+const routes = {
+  "/": "./home.html",
+  "/service": "./service.html",
+  "/about": "./about.html",
+  "/contact": "./contact.html",
+};
+
+/**
+ * @function route
+ * @description is called when a user clicks on a link that should trigger a new page in the application.
+ * @param object
+ * @returns void
+ */
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("pageContent").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
